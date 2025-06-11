cea95e17e3ca550e49ddcbbcd5238b32b75f8381
Normal <166686421+Vishnugoat@users.noreply.github.com>
2025-06-11 20:43:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.08%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,6 +1,14 @@
+from typing import List
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        n = len(nums)
+        for i in range(n):
+            indix = i
+            for j in range(i+1, n):
+                if nums[j] < nums[indix]:  # Fix comparison reference
+                    indix = j
+            nums[i], nums[indix] = nums[indix], nums[i]  # Swap correctly
