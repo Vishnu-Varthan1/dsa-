6bb9fcc5b485e7d7105f8ee2592f58a4ab0c2b44
vishnuvarthan <166686421+Vishnu-Varthan1@users.noreply.github.com>
2025-10-07 08:48:31 +0530

    Time: 1 ms (26.86%) | Memory: 41.3 MB (20.44%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxBottlesDrunk(int x, int y) {
+        int ans = x ;
+        while(x>=y){
+            x-= y-1;
+            y++;
+            ans++;
+        }
+        return ans;
+    }
+}
